16900b6054c41e929ced568cc2349ac4b4daac8b
Devang Kamble <risingentropy20@gmail.com>
2021-05-30 09:33:06 +0530

    get csv

--- updateData.py
+++ updateData.py
@@ -1,22 +1,34 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+import csv
 
-cred = credentials.Certificate("./serviceAccountKey.json")
-default_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+# cred = credentials.Certificate("./serviceAccountKey.json")
+# default_app = firebase_admin.initialize_app(cred)
+# db = firestore.client()
 
-doc_ref = db.collection(u'Students')
-docs = doc_ref.get()
+# doc_ref = db.collection(u'Students')
+# docs = doc_ref.get()
 
 
 PRNMapID = {}
 
-for doc in docs:
-    if doc.exists:
-        studentInstance = doc.to_dict()
-        PRNMapID[studentInstance['prn']] = studentInstance['userID']
-    else:
-        print(u'No such document!')
+# for doc in docs:
+#     if doc.exists:
+#         studentInstance = doc.to_dict()
+#         PRNMapID[studentInstance['prn']] = studentInstance['userID']
+#     else:
+#         print(u'No such document!')
+
+dic = {}
         
-print(PRNMapID)
+with open('GPA.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        lst = []
+        for i in range(1, len(row)-1):
+            lst.append(row[i])
+        dic[row[0]] = lst
+        
+print(dic)
+# print(PRNMapID)
